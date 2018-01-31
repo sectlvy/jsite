f8e19497c4f0000dd1ca0e6fca0501b05f439aef
lkl <531751008@qq.com>
2018-01-31 23:36:52 +0800

    生成代码

---
+++ db/createtable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `biz_equipment` (
+  `id` varchar(64) COLLATE utf8_bin NOT NULL COMMENT '编号',
+  `create_by` varchar(64) COLLATE utf8_bin NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) COLLATE utf8_bin NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) COLLATE utf8_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='设备';
